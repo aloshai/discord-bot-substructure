26aa079f5beaa275cb09f32185681a5405a2d3fb
aloshai <39468199+aloshai@users.noreply.github.com>
2020-12-24 08:20:32 +0300

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,4 +1,8 @@
+const {Client} = require("discord.js");
 const Config = require("../config/config.json");
+/**
+  * @type {Client} client
+  */
 const client = global.Client;
 
-client.login(Config.Token).catch(console.error);
+client.login(Config.Client.Token).catch(console.error);
